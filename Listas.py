c20b91d5968d39dc3bb630bc590026594c364e5c
FilipeUhlig <136847439+FilipeUhlig@users.noreply.github.com>
2023-06-23 15:58:40 -0300

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,43 @@
+"""
+Programa: listas
+Descrição: Este programa apresenta a estrutura de dados "listas" em Python para fins de aprendizado.
+Autor: F
+Data : 23/06/2023
+Versão: 0.0.1
+"""
+
+#Atribuição de variáveis
+
+# Criação de listas
+
+lista1 = [1, 'a', 2.3]
+
+b = [1, 2, 3, 4, 5, 6]
+
+e = [2, 13, 6, 1, 9]
+#Entrada de dados
+
+
+
+
+#Processamento de dados
+
+b[2] = 15
+c = sorted(b)
+
+
+
+
+#Saida de dadaos
+
+print(lista1)
+
+print(b)
+
+print(lista1[0])
+
+print(b[2])
+
+print(c)
+print(e.sort())
+print(e)
